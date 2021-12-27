ad40072643250edb56ad748551847d43e6559bbd
douglasmatosdev <douglasmatosdev@gmail.com>
2021-12-27 20:10:26 -0300

    add comment

--- course-workspace/curso_programacao/src/secao_04/exercicios/ExSomaSimples.java
+++ course-workspace/curso_programacao/src/secao_04/exercicios/ExSomaSimples.java
@@ -2,6 +2,16 @@ package secao_04.exercicios;
 
 import java.util.Scanner;
 
+/**
+ * 
+ * @author douglas
+ *
+ *	Faça um programa para ler dois valores inteiros,
+ *	e depois mostrar na tela a soma desses números com uma
+ *	mensagem explicativa, conforme exemplos.
+ *
+ */
+
 public class ExSomaSimples {
 
 	public static void run() {
