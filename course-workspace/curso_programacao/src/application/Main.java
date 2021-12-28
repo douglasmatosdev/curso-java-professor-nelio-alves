1bdd4d7bd0a424ac0e40e50d64e8eea6c049b1b0
douglasmatosdev <douglasmatosdev@gmail.com>
2021-12-28 19:17:23 -0300

    make exercice Telefonia

--- course-workspace/curso_programacao/src/application/Main.java
+++ course-workspace/curso_programacao/src/application/Main.java
@@ -1,11 +1,12 @@
 package application;
 
-import secao_03.Secao_03;
+import secao_05.Secao_05;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Secao_03.main();
+		Secao_05.main();
+//		Secao_03.main();
 //;		Secao_04.main();
 
 	}
---
+++ course-workspace/curso_programacao/src/secao_05/Secao_05.java
@@ -0,0 +1,10 @@
+package secao_05;
+
+import secao_05.telefonia.ProgramTelefonia;
+
+public class Secao_05 {
+
+	public static void main() {
+		ProgramTelefonia.run();
+	}
+}
