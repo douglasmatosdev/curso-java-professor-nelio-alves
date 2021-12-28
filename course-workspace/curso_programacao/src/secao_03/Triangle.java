f7b78d68d8431eb6a3aa02ee646b56b352a57ca0
douglasmatosdev <douglasmatosdev@gmail.com>
2021-12-28 00:12:29 -0300

    Add comment

--- course-workspace/curso_programacao/src/secao_03/Triangle.java
+++ course-workspace/curso_programacao/src/secao_03/Triangle.java
@@ -1,5 +1,23 @@
 package secao_03; // pacote da classe
 
+/**
+ * 
+ * @author douglas
+ *
+ *	Discussão
+ *
+ *	Quais são os benefícios de se calcular a área de um triângulo 
+ *	por meio de um MÉTODO dentro da CLASSE Triangle?
+ *
+ *	1) Reaproveitamento de código: nós eliminamos o código repetido
+ *	(cálculo das áreas dos triângulos x e y) no programa principal.
+ *
+ *	2) Delegação de responsabilidades: quem deve ser responsável por
+ *	saber como calcular a área de um triângulo é o próprio triângulo.
+ *	A lógica do cálculo da área não deve estar em outro lugar.
+ *
+ */
+
 public class Triangle {
 
 	/*
