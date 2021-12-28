ccddf4c646b78b20756440dd4c724787664daf49
douglasmatosdev <douglasmatosdev@gmail.com>
2021-12-28 00:33:57 -0300

    Product in stock

--- course-workspace/curso_programacao/src/secao_03/Secao_03.java
+++ course-workspace/curso_programacao/src/secao_03/Secao_03.java
@@ -1,11 +1,13 @@
 package secao_03;
 
+import secao_03.productstock.ProgramProductStock;
 import secao_03.triangle.ProgramTriangle;
 
 public class Secao_03 {
 
 	public static void main() {
 //		AreaTriangulo.run();
-		ProgramTriangle.run();
+//		ProgramTriangle.run();
+		ProgramProductStock.run();
 	}
 }
