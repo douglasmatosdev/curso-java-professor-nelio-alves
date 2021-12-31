de9eb274f97a9d01e5c09eaf7ef1499e679f593a
douglasmatosdev <douglasmatosdev@gmail.com>
2021-12-30 21:10:38 -0300

    Exercice - Rent rooms

--- course-workspace/curso_programacao/src/secao_10/Secao_10.java
+++ course-workspace/curso_programacao/src/secao_10/Secao_10.java
@@ -1,9 +1,12 @@
 package secao_10;
 
+import secao_10.exercices.ExRentBedroom;
+
 public class Secao_10 {
 
 	public static void main() {
 //		ProgramAverageHigher.run();
-		ProgramAveragePrice.run();
+//		ProgramAveragePrice.run();
+		ExRentBedroom.run();
 	}
 }
