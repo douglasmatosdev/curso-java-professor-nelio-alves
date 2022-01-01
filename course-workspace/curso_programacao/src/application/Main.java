2e72fd1975abe1fc0e7b6197f977be82a859663b
douglasmatosdev <douglasmatosdev@gmail.com>
2022-01-01 19:36:17 -0300

    Date - SimpleDateFormat

--- course-workspace/curso_programacao/src/application/Main.java
+++ course-workspace/curso_programacao/src/application/Main.java
@@ -1,11 +1,14 @@
 package application;
 
-import secao_10.Secao_10;
+import java.text.ParseException;
+
+import secao_11.Secao_11;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Secao_10.main();
+	public static void main(String[] args) throws ParseException {
+		Secao_11.main();
+//		Secao_10.main();
 //		Secao_09.main();
 //		Secao_07.main();
 //		Secao_05.main();
---
+++ course-workspace/curso_programacao/src/secao_11/Secao_11.java
@@ -0,0 +1,10 @@
+package secao_11;
+
+import java.text.ParseException;
+
+public class Secao_11 {
+
+	public static void main() throws ParseException {
+		ProgramDate.run();
+	}
+}
