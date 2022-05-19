d5113f38a483326de9a1715d5da512478b1314e3
douglasmatosdev <douglasmatosdev@gmail.com>
2022-05-19 11:16:56 -0300

    change directory and make exercices by section fourteen

--- intellij-workspace/src/secao_14/Secao_14.java
+++ intellij-workspace/src/secao_14/Secao_14.java
@@ -1,10 +1,13 @@
 package secao_14;
 
 import secao_14.bank.application.Program;
+import secao_14.exe1.application.ProgramExe1;
 
 public class Secao_14 {
 
     public static void main() {
-        Program.run();
+//        Program.run();
+        ProgramExe1.run();
     }
+
 }
---
+++ intellij-workspace/src/secao_14/bank/application/Program.java
@@ -0,0 +1,60 @@
+package secao_14.bank.application;
+
+import secao_14.bank.entities.Account;
+import secao_14.bank.entities.BusinessAccount;
+import secao_14.bank.entities.SavingsAccount;
+
+public class Program {
+
+    public static void run() {
+
+        Account x = new Account(1020, "Alex", 1000.0);
+        Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
+
+        x.withDraw(50.0);
+        y.withDraw(50.0);
+
+        System.out.println(x.getBalance());
+        System.out.println(y.getBalance());
+
+//        Account acc6 = new Account(1006, "Alexander", 1000.0);
+//        acc6.withDraw(200.0);
+//        System.out.println("acc6: "+ acc6.getBalance());
+//
+//        Account acc8 = new BusinessAccount(1008, "Alexander", 1000.0, 500.0);
+//        acc8.withDraw(200.0);
+//        System.out.println("acc8: "+acc8.getBalance());
+//
+//        Account acc = new Account(1001, "Alex", 0.0);
+//        BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+        // UPCASTING and OVERRIDE
+//        Account acc7 = new SavingsAccount(1007, "Alexander", 1000.0, 0.01);
+//        acc7.withDraw(200.0);
+//        System.out.println("acc7: "+ acc7.getBalance());
+//
+//        Account acc1 = acc;
+//        Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+//        Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+
+        // DOWNCASTING
+//
+//        BusinessAccount acc4 = (BusinessAccount) acc2;
+//        acc4.loan(100.0);
+
+        // Should error
+        // BusinessAccount acc5 = (BusinessAccount) acc3;
+        // Correct
+//        if (acc3 instanceof BusinessAccount) {
+//            BusinessAccount acc5 = (BusinessAccount) acc3;
+//            acc5.loan(200.0);;
+//            System.out.println("Loan!");
+//        }
+//
+//        if (acc3 instanceof SavingsAccount) {
+//            SavingsAccount acc5 = (SavingsAccount) acc3;
+//            acc5.updateBalance();
+//            System.out.println("Update!");
+//        }
+    }
+}
