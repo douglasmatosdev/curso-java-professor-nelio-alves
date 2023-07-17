8cd934caa09cbfa3fe2b17007e2f214b821910b6
douglasmatosdev <douglasmatosdev@gmail.com>
2023-07-17 15:06:55 -0300

    limpando o buffer de leitura

--- 04_section-estrutura-sequencial/workspace/entrada_de_dados_parte_dois/src/Main.java
+++ 04_section-estrutura-sequencial/workspace/entrada_de_dados_parte_dois/src/Main.java
@@ -5,12 +5,16 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         String s1, s2, s3;
+        int x;
 
+        x = sc.nextInt();
+        sc.nextLine();
         s1 = sc.nextLine();
         s2 = sc.nextLine();
         s3 = sc.nextLine();
 
         System.out.println("DADOS DIGITADOS: ");
+        System.out.println(x);
         System.out.println(s1);
         System.out.println(s2);
         System.out.println(s3);
