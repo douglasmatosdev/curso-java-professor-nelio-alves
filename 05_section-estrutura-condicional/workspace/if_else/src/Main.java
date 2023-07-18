a8ff4e853d71f48ff08d088d736d48a48d1fab73
douglasmatosdev <douglasmatosdev@gmail.com>
2023-07-18 16:22:43 -0300

    IF ELSE

---
+++ 05_section-estrutura-condicional/workspace/if_else/src/Main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+/*
+        // 1° EXEMPLO
+        int x = 5;
+
+        System.out.println("Bom dia");
+
+        if (x < 0) {
+            System.out.println("Boa tarde");
+        }
+
+        System.out.println("Boa noite");
+
+ */
+/*
+        // 2° EXEMPLO
+        Scanner sc = new Scanner(System.in);
+        int hora;
+
+        System.out.println("Quantas horas?");
+        hora = sc.nextInt();
+
+        if (hora < 12) {
+            System.out.println("Bom dia");
+        } else {
+            System.out.println("Boa tarde");
+        }
+        sc.close();
+        */
+
+        Scanner sc = new Scanner(System.in);
+        int hora;
+
+        System.out.println("Quantas horas?");
+        hora = sc.nextInt();
+
+        if (hora < 12) {
+            System.out.println("Bom dia");
+        } else if (hora < 18) {
+            System.out.println("Boa tarde");
+        } else {
+            System.out.println("Boa noite");
+        }
+        sc.close();
+
+    }
+}
