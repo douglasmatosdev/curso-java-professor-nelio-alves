31a8526145899807a93374fa51e3bfb0cd7c0238
douglasmatosdev <douglasmatosdev@gmail.com>
2023-07-23 12:36:23 -0300

    section 8 - membros estaticos parte 2

---
+++ 08_section-introducao-a-programacao-orientada-a-objetos/workspace/membros-estaticos-parte-2/src/util/Calculator.java
@@ -0,0 +1,13 @@
+package util;
+
+public class Calculator {
+    public final double PI = 3.14159;
+
+    public double circumgerence(double radius) {
+        return 2 * PI * radius;
+    }
+
+    public double volume(double radius) {
+        return 4.0 * PI * radius * radius * radius / 3.0;
+    }
+}
