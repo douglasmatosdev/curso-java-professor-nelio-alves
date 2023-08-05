efaef079a5651705f97216ec1d3ce7f50647ee8f
douglasmatosdev <douglasmatosdev@gmail.com>
2023-08-05 19:26:12 -0300

    exercices

---
+++ 04_section-estrutura-sequencial/workspace/maven-exemplo-projeto/src/test/java/AppTeste.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+import junit.framework.Assert;
+public class AppTeste {
+    @Test
+    public void test() {
+        Assert.assertEquals("Este é o segundo exemplo!", new String("Este é o segundo exemplo!"));
+    }
+}
