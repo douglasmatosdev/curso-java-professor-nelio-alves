a135da5442f91b0d147e12dcaca1f48c006c8a52
douglasmatosdev <douglasmatosdev@gmail.com>
2023-08-20 13:11:34 -0300

    section 9 - encapsulamento, getters and sestters

---
+++ 09_section-construtores-palavra-this-sobrecarga-encapsulamento/workspace/Exemplo3/src/entities/Product.java
@@ -0,0 +1,63 @@
+package entities;
+
+public class Product {
+	private String name;
+	private double price;
+	private int quantity;
+	
+	public Product() {
+		
+	}
+	
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+		/*por padrão a inicilização sempre será zero, por isso é opcional setar o valor zero*/
+//		this.quantity = 0;
+//		quantity = 0;
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Double getPrice() {
+		return price;
+	}
+	
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+	
+	public double totalValueInStock() {
+		return price * quantity;
+	}
+
+	public void addProducts(int quantity) {
+		this.quantity += quantity;
+	}
+
+	public void removeProducts(int quantity) {
+		this.quantity -= quantity;
+	}
+
+	public String toString() {
+		return name + ", $ " + String.format("%.2f", price) + ", " + quantity + " units, Total: $ "
+				+ String.format("%.2f", totalValueInStock());
+	}
+}
