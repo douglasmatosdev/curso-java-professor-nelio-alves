b9b0538bd478295aa2c976afcb416eed5f2203d9
douglasmatosdev <douglasmatosdev@gmail.com>
2023-08-20 16:48:33 -0300

    section 10 - desalocação de memoria

---
+++ 10_section-comportamento-de-memoria-arrays-listas/workspace/Exemplo1/src/application/Program.java
@@ -0,0 +1,18 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// Syntax error, insert "VariableDeclarators" to complete
+		// LocalVariableDeclaration
+		// int p;
+		// p; //<- vai dar erro pois a variável não foi inicializada
+		// System.out.println(p);
+
+		int p;
+		p = 10;
+		System.out.println(p);
+
+	}
+
+}
