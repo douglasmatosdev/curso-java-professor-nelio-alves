6367c752ddf5f081d9e5a085ce2cffc5b73be2b8
douglasmatosdev <douglasmatosdev@gmail.com>
2024-02-19 16:09:10 -0300

    section 18 - Herança multipla e o problema do diamante

---
+++ 18_section-interfaces/workspace/Example5DiamantProblem/src/devices/ComboDevice.java
@@ -0,0 +1,23 @@
+package devices;
+
+public class ComboDevice extends Device implements Printer, Scanner {
+
+    public ComboDevice(String serialNumber) {
+        super(serialNumber);
+    }
+
+    @Override
+    public void processDoc(String doc) {
+        System.out.println("Combo processing: " + doc);
+    }
+
+    @Override
+    public void print(String doc) {
+        System.out.println("Combo printing: " + doc);
+    }
+
+    @Override
+    public String scan() {
+        return "Combo scan result";
+    }
+}
