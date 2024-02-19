b2ad4bc4d6447856534614881da43fc740fe426e
douglasmatosdev <douglasmatosdev@gmail.com>
2024-02-19 11:25:21 -0300

    section 18 - exercicio de interface

---
+++ 18_section-interfaces/workspace/Exercise1/src/application/Program.java
@@ -0,0 +1,49 @@
+package application;
+
+import entities.Contract;
+import entities.Installment;
+import services.ContractService;
+import services.PaypalService;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.println("Entre com os dados do contrato");
+
+        System.out.print("Numero: ");
+        int number = sc.nextInt();
+
+        System.out.print("Data (dd/MM/yyyy): ");
+        LocalDate date = LocalDate.parse(sc.next(), dtf);
+
+        System.out.print("Valor do contrato: ");
+        double totalValue = sc.nextDouble();
+
+        Contract contract = new Contract(number, date, totalValue);
+
+        System.out.print("Entre com o numero de parcelas: ");
+        int n = sc.nextInt();
+
+        ContractService contractService = new ContractService(new PaypalService());
+        contractService.processContract(contract, n);
+
+        System.out.println("Parcelas");
+
+        for (Installment installment : contract.getInstallments()) {
+            System.out.println(installment);
+        }
+
+        sc.close();
+
+    }
+}
