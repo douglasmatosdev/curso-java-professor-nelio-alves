9ef8f6b477213b3f659d4f486336f3ad8ec0a0d9
douglasmatosdev <douglasmatosdev@gmail.com>
2024-06-15 19:54:39 -0300

    section 16 - Chess layer and printing the board

---
+++ 15_section-tratamento-de-excecoes/workspace/ChessSystemJava/src/main/java/org/chess/ChessPiece.java
@@ -0,0 +1,17 @@
+package org.chess;
+
+import org.boardgame.Board;
+import org.boardgame.Piece;
+
+public class ChessPiece extends Piece {
+    private Color color;
+
+    public ChessPiece(Board board, Color color) {
+        super(board);
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+}
---
+++ 15_section-tratamento-de-excecoes/workspace/ChessSystemJava/src/main/java/org/chess/Color.java
@@ -0,0 +1,6 @@
+package org.chess;
+
+public enum Color {
+    BLACK,
+    WHITE
+}
