9d68b203bf5ea773860834f6ec9e5c987f0d21db
douglasmatosdev <douglasmatosdev@gmail.com>
2024-06-16 11:56:35 -0300

    Handling exceptions and clearing screen

---
+++ 16_section-projeto-sistema-de-xadrez/workspace/ChessSystemJava/src/main/java/org/application/Program.java
@@ -0,0 +1,40 @@
+package org.application;
+
+import org.chess.ChessException;
+import org.chess.ChessMatch;
+import org.chess.ChessPiece;
+import org.chess.ChessPosition;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        ChessMatch chessMatch = new ChessMatch();
+
+        while (true) {
+            try {
+
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
+
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
+
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            } catch (ChessException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+        }
+    }
+}
