c5ee4a06b17656e23c034049e5e06d119b315b52
douglasmatosdev <douglasmatosdev@gmail.com>
2024-06-16 14:53:44 -0300

    section 16 - Printing possible moves

--- 16_section-projeto-sistema-de-xadrez/workspace/ChessSystemJava/src/main/java/org/application/Program.java
+++ 16_section-projeto-sistema-de-xadrez/workspace/ChessSystemJava/src/main/java/org/application/Program.java
@@ -16,13 +16,15 @@ public class Program {
 
         while (true) {
             try {
-
                 UI.clearScreen();
                 UI.printBoard(chessMatch.getPieces());
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
 
+                boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces(), possibleMoves);
                 System.out.println();
                 System.out.print("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
