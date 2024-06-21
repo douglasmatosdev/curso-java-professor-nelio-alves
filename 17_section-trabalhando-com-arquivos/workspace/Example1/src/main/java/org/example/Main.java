f3e24b1653079643d499a57f9c1cdc2cd671dfba
douglasmatosdev <douglasmatosdev@gmail.com>
2024-06-21 18:36:30 -0300

    section 17 - lendo arquivo de texto com as classes File e Scanner

---
+++ 17_section-trabalhando-com-arquivos/workspace/Example1/src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        File file = new File("/tmp/in.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            if (sc != null) {
+                sc.close();
+            }
+        }
+    }
+}
