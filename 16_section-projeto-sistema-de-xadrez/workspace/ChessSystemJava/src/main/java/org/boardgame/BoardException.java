644c706224d5b53b6adfa8d4a6f7e98d0af661e2
douglasmatosdev <douglasmatosdev@gmail.com>
2024-06-21 18:20:04 -0300

    section 16 - finish project chess

--- 16_section-projeto-sistema-de-xadrez/workspace/ChessSystemJava/src/main/java/org/boardgame/BoardException.java
+++ 16_section-projeto-sistema-de-xadrez/workspace/ChessSystemJava/src/main/java/org/boardgame/BoardException.java
@@ -1,7 +1,9 @@
 package org.boardgame;
 
 public class BoardException extends RuntimeException {
-    public BoardException(String message) {
-        super(message);
-    }
+	private static final long serialVersionUID = 1L;
+
+	public BoardException(String msg) {
+		super(msg);
+	}
 }
