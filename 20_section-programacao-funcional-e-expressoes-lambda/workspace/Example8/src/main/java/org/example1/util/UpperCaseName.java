fa9ff7cac2be2bb2a08b31c736c38f44050765e3
douglasmatosdev <douglasmatosdev@gmail.com>
2024-06-23 12:51:46 -0300

    section 20 - Function

---
+++ 20_section-programacao-funcional-e-expressoes-lambda/workspace/Example8/src/main/java/org/example1/util/UpperCaseName.java
@@ -0,0 +1,12 @@
+package org.example1.util;
+
+import org.example1.entities.Product;
+
+import java.util.function.Function;
+
+public class UpperCaseName implements Function<Product, String> {
+    @Override
+    public String apply(Product product) {
+        return product.getName().toUpperCase();
+    }
+}
---
+++ 20_section-programacao-funcional-e-expressoes-lambda/workspace/Example8/src/main/java/org/example3/Program.java
@@ -0,0 +1,24 @@
+package org.example3;
+
+import org.example3.entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Program {
+    public static void main(String[] args) {
+        List<Product> list = new ArrayList<>();
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        List<String> names = list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
+
+        names.forEach(System.out::println);
+
+        names.forEach(System.out::println);
+    }
+}
