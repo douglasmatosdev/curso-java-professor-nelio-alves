fd54386d53e518f8111bb93ede56dc6778c1a4e8
douglasmatosdev <douglasmatosdev@gmail.com>
2024-07-06 11:35:28 -0300

    section 21 - SellerDaoJDBC and DaoFactory

--- 21_section-acesso-a-banco-de-dados-com-jdbc/workspace/demo-dao-jdbc/src/application/Program.java
+++ 21_section-acesso-a-banco-de-dados-com-jdbc/workspace/demo-dao-jdbc/src/application/Program.java
@@ -1,5 +1,7 @@
 package src.application;
 
+import src.model.dao.DaoFactory;
+import src.model.dao.SellerDao;
 import src.model.entities.Department;
 import src.model.entities.Seller;
 
@@ -9,6 +11,7 @@ public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
         Seller seller = new Seller(21, "Bob", "bob@email.com", new Date(), 3000.0, obj);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println(obj);
         System.out.println(seller);
     }
