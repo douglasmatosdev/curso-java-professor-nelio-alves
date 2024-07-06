fdbc24ba3d2bb84542429da409d74fed57893e99
douglasmatosdev <douglasmatosdev@gmail.com>
2024-07-06 15:25:48 -0300

    section 21 - insert implementation

--- 21_section-acesso-a-banco-de-dados-com-jdbc/workspace/demo-dao-jdbc/src/application/Program.java
+++ 21_section-acesso-a-banco-de-dados-com-jdbc/workspace/demo-dao-jdbc/src/application/Program.java
@@ -5,6 +5,7 @@ import src.model.dao.SellerDao;
 import src.model.entities.Department;
 import src.model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 
@@ -23,10 +24,15 @@ public class Program {
             System.out.println(obj);
         }
 
-        System.out.println("\n=== TEST 2: seller findAll ===");
+        System.out.println("\n=== TEST 3: seller findAll ===");
         list = sellerDao.findAll();
         for (Seller obj : list) {
             System.out.println(obj);
         }
+
+        System.out.println("\n=== TEST 4: seller insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
     }
 }
