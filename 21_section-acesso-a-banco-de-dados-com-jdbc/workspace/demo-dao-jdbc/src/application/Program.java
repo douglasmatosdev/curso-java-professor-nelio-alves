ba6002656edfb63996c7f7913bb7d05577aadfcb
douglasmatosdev <douglasmatosdev@gmail.com>
2024-07-06 11:49:49 -0300

    section 21 - findById implementation

--- 21_section-acesso-a-banco-de-dados-com-jdbc/workspace/demo-dao-jdbc/src/application/Program.java
+++ 21_section-acesso-a-banco-de-dados-com-jdbc/workspace/demo-dao-jdbc/src/application/Program.java
@@ -9,10 +9,8 @@ import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Department obj = new Department(1, "Books");
-        Seller seller = new Seller(21, "Bob", "bob@email.com", new Date(), 3000.0, obj);
         SellerDao sellerDao = DaoFactory.createSellerDao();
-        System.out.println(obj);
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
     }
 }
--- 21_section-acesso-a-banco-de-dados-com-jdbc/workspace/demo-dao-jdbc/src/model/dao/DaoFactory.java
+++ 21_section-acesso-a-banco-de-dados-com-jdbc/workspace/demo-dao-jdbc/src/model/dao/DaoFactory.java
@@ -1,9 +1,10 @@
 package src.model.dao;
 
+import src.db.DB;
 import src.model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
     public static SellerDao createSellerDao() {
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.getConnection());
     }
 }
