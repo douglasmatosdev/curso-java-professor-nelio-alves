034cb7b3f0bf8ea1a04d58f5ff5aa0bdc99de763
douglasmatosdev <douglasmatosdev@gmail.com>
2024-07-06 15:36:27 -0300

    section 21 - delete implementation

--- 21_section-acesso-a-banco-de-dados-com-jdbc/workspace/demo-dao-jdbc/src/application/Program.java
+++ 21_section-acesso-a-banco-de-dados-com-jdbc/workspace/demo-dao-jdbc/src/application/Program.java
@@ -7,10 +7,13 @@ import src.model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 
 public class Program {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println("=== TEST 1: seller findById ===");
@@ -40,5 +43,13 @@ public class Program {
         seller.setName("Martha Waine");
         sellerDao.update(seller);
         System.out.println("Update completed");
+
+        System.out.println("\n=== TEST 6: seller delete ===");
+        System.out.print("Enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed");
+
+        sc.close();
     }
 }
