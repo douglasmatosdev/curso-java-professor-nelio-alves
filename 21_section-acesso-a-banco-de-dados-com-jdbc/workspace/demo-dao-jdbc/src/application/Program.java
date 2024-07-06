0e450ff968b5d0e6de1778a7c78c9a2009915dec
douglasmatosdev <douglasmatosdev@gmail.com>
2024-07-06 11:58:43 -0300

    section 21 - Reusing instantiation

--- 21_section-acesso-a-banco-de-dados-com-jdbc/workspace/demo-dao-jdbc/src/application/Program.java
+++ 21_section-acesso-a-banco-de-dados-com-jdbc/workspace/demo-dao-jdbc/src/application/Program.java
@@ -2,14 +2,14 @@ package src.application;
 
 import src.model.dao.DaoFactory;
 import src.model.dao.SellerDao;
-import src.model.entities.Department;
 import src.model.entities.Seller;
 
-import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        System.out.println("=== TEST 1: seller findById ===");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
     }
