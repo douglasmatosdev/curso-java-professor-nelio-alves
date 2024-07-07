31ef834210c8fd6634f89983e3c990aff1ee4d45
douglasmatosdev <douglasmatosdev@gmail.com>
2024-07-07 19:30:20 -0300

    section 22 - primeiro teste

--- 22_section-java-ee-mapeamento-objeto-relacional-com-jpa-hibernate/workspace/aulajpamaven/src/main/java/com/douglasmatosdev/aplicativo/Programa.java
+++ 22_section-java-ee-mapeamento-objeto-relacional-com-jpa-hibernate/workspace/aulajpamaven/src/main/java/com/douglasmatosdev/aplicativo/Programa.java
@@ -2,14 +2,30 @@ package com.douglasmatosdev.aplicativo;
 
 import com.douglasmatosdev.dominio.Pessoa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Programa {
     public static void main(String[] args) {
 
-        Pessoa p1 = new Pessoa(1, "Carlos da Silva", "carlos@gmail.com");
-        Pessoa p2 = new Pessoa(2, "Joaquim Torres", "joaquim@gmail.com");
-        Pessoa p3 = new Pessoa(3, "Ana Maria", "ana@gmail.com");
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
+        Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+        Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
+        Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        em.persist(p1);
+        em.persist(p2);
+        em.persist(p3);
+
+        em.getTransaction().commit();
+
+        emf.close();
+        em.close();
+        System.out.println("Pronto!");
     }
 }
--- 22_section-java-ee-mapeamento-objeto-relacional-com-jpa-hibernate/workspace/aulajpamaven/src/main/java/com/douglasmatosdev/dominio/Pessoa.java
+++ 22_section-java-ee-mapeamento-objeto-relacional-com-jpa-hibernate/workspace/aulajpamaven/src/main/java/com/douglasmatosdev/dominio/Pessoa.java
@@ -1,8 +1,15 @@
 package com.douglasmatosdev.dominio;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
+@Entity
 public class Pessoa implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String email;
