ca469d7a8ed7681a03bcf530fa158ee19fd731fe
douglasmatosdev <douglasmatosdev@gmail.com>
2024-07-07 20:08:48 -0300

    section 23 - User entity and resource

---
+++ 23_section-projeto-web-services-com-spring-boot-e-jpa-hibernate/workspace/course/src/main/java/com/douglasmatosdev/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.douglasmatosdev.course.resources;
+
+import com.douglasmatosdev.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "1234");
+
+        return ResponseEntity.ok().body(u);
+    }
+}
