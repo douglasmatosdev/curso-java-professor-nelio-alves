d1609929fa6454484f8e6fafd79bf2f52eae1b7e
douglasmatosdev <douglasmatosdev@gmail.com>
2024-11-03 16:08:26 -0300

    Exception handler - findById

--- 22_section-projeto-web-services-com-spring-boot-e-jpa-hibernate/workspace/course/src/main/java/com/douglasmatosdev/course/services/UserService.java
+++ 22_section-projeto-web-services-com-spring-boot-e-jpa-hibernate/workspace/course/src/main/java/com/douglasmatosdev/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.douglasmatosdev.course.services;
 
 import com.douglasmatosdev.course.entities.User;
 import com.douglasmatosdev.course.repositories.UserRepository;
+import com.douglasmatosdev.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
